09760c0e8ae23f19a9086ae877f400b5dcca79be
Yagnaguru Ravichandran <yagnaguru.r@listertechnologies.com>
2018-07-22 23:38:14 +0530

    recent post changes

--- js/content.js
+++ js/content.js
@@ -1,17 +1,23 @@
 var base_link = "D:/YagnaGuru/blog/"
 var topics = [{
-    heading : " Type Erasure and Synthetic Bridge methods in Generics",
+    heading : "Type Erasure and Synthetic Bridge methods in Generics",
     intro: "Generics in Java helps programer to write a code in a generic way and it also ensure to have tighter type checks during compile time",
-    link: base_link + "/java/type_erasure.html",
+    link: "./java/type_erasure.html",
     image: "https://cdn.pixabay.com/photo/2015/12/09/14/51/letter-1084827_960_720.png",
     timeDesc:"July 2018",
     dateTime: "2018-07"
 }];
 
+var recentPost = [{
+    desc: "Type Erasure and Synthetic Bridge methods in Generics",
+    tag: "java",
+    page:"type_erasure.html"
+}];
+
 $(function(){
 
-    $("#brandNameLink").attr("href", base_link+"index.html");
-    $("#javaLink").attr("href", base_link+"index.html");
+    //$("#brandNameLink").attr("href", "../index.html");
+    //$("#javaLink").attr("href", "../index.html");
 
     var i = 0;
     for( var i =0; i< topics.length; i++ ){
@@ -27,4 +33,17 @@ $(function(){
         var contentSec = $("<div  class='col-sm-12 content-sec'>").html(imgRow);
         $("#scrollIndex").append(contentSec);   
     }
+
+    var title = "<h5 class='card-title'><strong>Recent Post</strong></h5>";
+    $("#recentPost").html(title);
+    var self = $("#selfNavigate").val() == "1";
+    
+    for( var i =0; i<recentPost.length; i++ ){
+        var ele = recentPost[i];
+        var link = self? "../"+ele.tag+"/"+ele.page : "./"+ele.tag+"/"+ele.page;
+        var con ="<p class='card-text'><a href='"+link+"'>"+ele.desc+".</a></p>";
+        $("#recentPost").html(con);
+    }
+
+
 });
